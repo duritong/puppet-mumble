a6b00a7fb6160cf7d9741bd74bd119ed30c11dab
Silvio Rhatto <rhatto@riseup.net>
2012-05-13 19:11:06 -0300

    Adding mumble::ssl

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,3 +19,20 @@ class mumble {
     mode    => 0640;
   }
 }
+
+class mumble::ssl inherits mumble {
+  file { [ "/etc/mumble-server", "/etc/mumble-server/ssl", "/etc/mumble-server/ssl/certs", "/etc/mumble-server/ssl/private" ]:
+    ensure  => directory,
+    owner   => "root",
+    group   => "mumble-server",
+    mode    => 0750,
+  }
+
+  ssl::cert { "cert":
+    owner   => 'mumble-server',
+    group   => 'mumble-server',
+    base    => '/etc/mumble-server/ssl',
+    notify  => Service['mumble-server'],
+    require => File['/etc/mumble-server/ssl/private'],
+  }
+}
