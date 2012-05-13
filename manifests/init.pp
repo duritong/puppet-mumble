a7d06c32ce26162e8233254477f167a5be4bed5f
Silvio Rhatto <rhatto@riseup.net>
2012-05-13 18:20:42 -0300

    Adding default config

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,4 +2,15 @@ class mumble {
   package { 'mumble-server':
     ensure => installed,
   }
+
+  file{'/etc/mumble-server.ini':
+    source => [ "puppet:///modules/site-mumble/${fqdn}/mumble-server.ini",
+                "puppet:///modules/site-mumble/mumble-server.ini",
+                "puppet:///modules/mumble/mumble-server.ini" ],
+    require => Package['mumble-server'],
+    notify  => Service['mumble-server'],
+    owner   => root,
+    group   => mumble-server,
+    mode    => 0640;
+  }
 }
