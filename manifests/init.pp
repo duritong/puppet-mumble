afd73d7508dd5b0823fa881302363b4d76d7f456
mh <mh@immerda.ch>
2017-02-20 22:47:25 +0100

    make the config a template

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,27 +4,26 @@
 #
 #    * config_content: get content for central config file from
 #      this parameter. Useful for using templates
-#    * config_source: Source path for your config
+#    * config: options to set, see supported options in template
 #    * manage_munin: deploy munin plugins?
 #
 class mumble(
   $config_content = false,
-  $config_source  = [ "puppet:///modules/site_mumble/${::fqdn}/mumble-server.ini",
-                      'puppet:///modules/site_mumble/mumble-server.ini',
-                      'puppet:///modules/mumble/mumble-server.ini' ],
+  $config         = {},
   $manage_munin   = false,
-  $icesecret      = 'secureme',
 ) {
   if $osfamily == 'RedHat' {
     $pkg_name = 'murmur'
     $service_name = 'murmur'
+    $config_file  = '/etc/murmur/murmur.ini'
   } else {
     $pkg_name = 'mumble-server'
     $service_name = 'mumble-server'
+    $config_file  = '/etc/mumble-server.ini'
   }
   package{$pkg_name:
     ensure => installed,
-  } -> file{'/etc/mumble-server.ini':
+  } -> file{$config_file:
     owner   => root,
     group   => mumble-server,
     mode    => '0640';
@@ -34,12 +33,12 @@ class mumble(
   }
 
   if $config_content {
-    File['/etc/mumble-server.ini']{
+    File[$config_file]{
       content => $config_content,
     }
   } else {
-    File['/etc/mumble-server.ini']{
-      source => $config_source,
+    File[$config_file]{
+      content => template('mumble/mumble-server.ini.erb'),
     }
   }
 
